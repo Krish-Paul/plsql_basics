bf0445f4fe890258235e94f342b92505dee03904
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-06 10:23:36 +0530

    Update bindvariable.sql

--- bindvariable.sql
+++ bindvariable.sql
@@ -5,9 +5,15 @@ Way_1. :-
 --------
 EXEC :V_NAME := 'Oracle Shooter';
 
+BEGIN
+dbms_output.put_line(:V_NAME);
+END;
+
+
 --------
 Way_2. :-
 --------
 BEGIN
 :V_NAME := 'Oracle Shooter';
+dbms_output.put_line(:V_NAME);
 END;
