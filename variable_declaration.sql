766a90e8308269f04416f6b1cb3f486a844c429f
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-06 11:02:50 +0530

    Update variable_declaration.sql

--- variable_declaration.sql
+++ variable_declaration.sql
@@ -1,3 +1,8 @@
 DECLARE
 vname VARCHAR(100);---simple declaration predefined type and size
 vsal EMPLYEES.SAL % type;-- dynamic declaration where the type and size of salary column in employees table is copied to vsal
+
+
+
+VAR_1  EMPLOYEES % ROWTYPE; --dynamically stores all the column size and types in one variable VAR_1... 
+----if employees has columns names,salary then VAR_1.name ;Var_1.SAL will store the size and type of those columns
