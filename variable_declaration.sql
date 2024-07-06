6a8d0d605911cfb0009c478cecaf6931c1c67cd0
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-06 10:48:44 +0530

    Create variable_declaration.sql

---
+++ variable_declaration.sql
@@ -0,0 +1,3 @@
+DECLARE
+vname VARCHAR(100);---simple declaration predefined type and size
+vsal EMPLYEES.SAL % type;-- dynamic declaration where the type and size of salary column in employees table is copied to vsal
