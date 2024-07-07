9bb5bc0746aaf28e01269dfbeb011b1540cdaebe
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-07 19:00:45 +0530

    Update loops.sql

--- loops.sql
+++ loops.sql
@@ -11,3 +11,14 @@ if x>10 then exit;
 end if;
 end loop;
 end;
+
+---simple loop with EXIT WHEN statement
+declare
+x number :=0;
+begin
+loop
+dbms_output.put_line(x);
+x:=x+1;
+exit when x>10;
+end loop;
+end;
