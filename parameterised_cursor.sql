978ece047a0e57f584557abaf04ae6bab24e962e
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-08 21:35:08 +0530

    Create parameterised_cursor.sql

---
+++ parameterised_cursor.sql
@@ -0,0 +1,75 @@
+---taking 1 parameter
+DECLARE
+V_NAME  XX_EMP_TL.ENAME%TYPE;
+V_SAL XX_EMP_TL.SAL%TYPE;
+
+   --Declare Cursor for all dept  --
+  CURSOR C1(P_DEPTNO XX_EMP_TL.DEPTNO%TYPE) IS
+  SELECT Ename, sal FROM XX_EMP_TL where DEPTNO = P_DEPTNO;
+ 
+BEGIN
+
+    OPEN C1(20);
+    dbms_output.put_line ('------------  Department 20 Employee information  ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+       
+    OPEN C1(30); -- Re-Use Above cursor for 30 department --
+    dbms_output.put_line ('------------  Department 30 Employee information  ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+   
+    OPEN C1(40); -- Re-Use Above cursor for 40 department --
+    dbms_output.put_line ('------------  Department 40 Employee information  ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+   
+END;
+
+
+DECLARE
+V_NAME  XX_EMP_TL.ENAME%TYPE;
+V_SAL XX_EMP_TL.SAL%TYPE;
+
+   --Declare Cursor for all dept  --
+  CURSOR C1(P_DEPTNO XX_EMP_TL.DEPTNO%TYPE, P_SAL XX_EMP_TL.SAL%TYPE) IS
+  SELECT Ename, sal
+  FROM XX_EMP_TL
+  where DEPTNO = P_DEPTNO
+  AND SAL > P_SAL;
+ 
+BEGIN
+    OPEN C1(20, 21000);
+    dbms_output.put_line ('------------  Department 20 Employee information where salary is more then  21000  ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+       
+    OPEN C1(30,10000); -- Re-Use Above cursor for 30 department  where salary is more then  10000 -----
+    dbms_output.put_line ('------------  Department 30 Employee information where salary is more then  10000  ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+   
+    OPEN C1(40,17000); -- Re-Use Above cursor for 40 department where salary is more then  17000--
+    dbms_output.put_line ('------------  Department 40 Employee information where salary is more then  17000   ------------');
+        LOOP FETCH C1 INTO V_NAME, V_SAL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_name||'  and Your Salary is : '||V_SAL ); 
+        END LOOP;
+    CLOSE C1;
+   
+END;
