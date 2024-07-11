0f966d970fd4d48e36d82cdfa96673e3d8882f3e
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-11 11:43:02 +0530

    Create delete_dupl_data.sql

---
+++ delete_dupl_data.sql
@@ -0,0 +1,28 @@
+create table XX_DELIVERY_DATA_TL (
+    id number primary key,
+    order_num  varchar2(500 byte),
+    trx_type  varchar2(500 byte),
+    vender varchar2(100 byte),
+    proc varchar2(5 byte)
+)
+select * from XX_DELIVERY_DATA_TL
+create sequence XX_DEL_SEQ
+
+ 
+INSERT INTO XX_DELIVERY_DATA_TL
+select XX_DEL_SEQ.NEXTVAL, '98981210001', 'Delivery Feed', 'Cun Mun', 'N' FROM DUAL
+ 
+INSERT INTO XX_DELIVERY_DATA_TL
+select XX_DEL_SEQ.NEXTVAL, '98981210002', 'Delivery Feed', 'Vishal maga Mart', 'N'  FROM DUAL
+ 
+INSERT INTO XX_DELIVERY_DATA_TL
+select XX_DEL_SEQ.NEXTVAL, '98981210003', 'Delivery Feed', 'India Pvt Ltd', 'N'  FROM DUAL
+
+SELECT MAX(ID) FROM XX_DELIVERY_DATA_TL where VENDER = 'Cun Mun'
+ GROUP BY ORDER_NUM, TRX_TYPE, VENDER,PROC;
+
+DELETE XX_DELIVERY_DATA_TL A
+--select * from  XX_DELIVERY_DATA_TL A
+WHERE 1=1
+AND A.VENDer = 'Cun Mun'
+AND ID NOT IN ( SELECT MIN(ID) FROM XX_DELIVERY_DATA_TL Z WHERE A.VENDer = Z.VENder  GROUP BY ORDER_NUM, TRX_TYPE, VENDer,PROC );         
